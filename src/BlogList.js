307b43507643b8be5bb1846a58e215c02d796a46
himanshuV09 <whoishimanshuverma@example.com>
2024-10-06 03:59:00 +0530

    some changes in code

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,8 +2,6 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const BlogList = ({ blogs, title }) => {
 
-    
-
     return ( 
         <div className="blog-list">
             <h2>{ title }</h2>
--- src/Create.js
+++ src/Create.js
@@ -1,63 +1,60 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 const Create = () => {
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
-    const [author, setAuthor] = useState('mario');
-    const [isPending, setIsPending] = useState(false);
-    const history = useHistory();
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const blog = { title, body, author };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
 
-        setIsPending(true);
-        
-        fetch('http://localhost:8000/blogs/', {
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(blog)
-        }).then(() => {
-            console.log('new blog added');
-            setIsPending(false);
-        })
+    setIsPending(true);
 
-        history.push('/');
-    }
+    fetch("http://localhost:8000/blogs/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+    });
 
-    return ( 
-        <div className="create">
-            <h2>Add a new Blog!</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Blog title: </label>
-                <input
-                    type="text"
-                    required
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-                <label>Blog body: </label>
-                <textarea
-                    required
-                    value={body}
-                    onChange={(e) => setBody(e.target.value)}
-                ></textarea>
-                <label>Blog author: </label>
-                <select
-                value={author}
-                onChange={(e) => setAuthor(e.target.value)}>
-                    <option value="sonam">sonam</option>
-                    <option value="tanu">tanu</option>
-                    <option value="shivam">shivam</option>
-                    <option value="vikas">vikas</option>
-                    <option value="anuj">anuj</option>
-                    <option value="vikas">vikas</option>
-                </select>
-                { !isPending && <button>Add Blog</button>}
-                { isPending && <button disabled>Adding Blog...</button>}
-            </form>
-        </div>
-     );
-}
- 
+    history.push("/");
+  };
+
+  return (
+    <div className="create">
+      <h2>Add a new Blog!</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title: </label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body: </label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author: </label>
+
+        <input
+          type="text"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog...</button>}
+      </form>
+    </div>
+  );
+};
+
 export default Create;
